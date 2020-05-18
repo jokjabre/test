5faec428596620fb36c323040f9cd31711b5f836
jok ja bre <jokjabre@outlook.com>
2020-05-18 19:39:53 +0200

    push

---
+++ UserNamer/PublishPages.ps1
@@ -0,0 +1,14 @@
+﻿$pathToSolution = "../"
+$projectName = "UserNamer"
+$repoName = "UserNamer"
+
+Write-Output "----==== Publish $pathToSolution"
+dotnet publish $pathToSolution -c Release -o ../dist/
+Write-Output ""
+
+Write-Output "----==== Copy from ../dist/wwwroot"
+Copy-Item -Path "../dist/wwwroot/*" -Destination "../docs" -Recurse -Force
+Write-Output ""
+
+Write-Output "----==== Delete dist folder"
+Remove-Item ../dist/ -Recurse
